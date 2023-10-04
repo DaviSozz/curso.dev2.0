3495d489b321e7867cbceda54b52b1e8b99732e1
DaviSozz <121989798+DaviSozz@users.noreply.github.com>
2023-10-04 10:12:37 -0300

    modificaçoes

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,5 +1,5 @@
-function status(request, reponse) {
-  Response.status(200).send("Alunos do curso.dev são pessoas acima da média");
+function status(request, response) {
+  response.status(200).json({ chave: "são acima da média " });
 }
 
 export default status;
